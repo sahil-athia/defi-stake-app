c3d2f7279b90320d9402304e0e998d9e745510d7
Sahil Athia <sahilathia918@gmail.com>
2021-02-23 16:27:37 -0500

    add token addresses to state vars

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,11 @@
 const Tokenfarm = artifacts.require("Tokenfarm");
+const DappToken = artifacts.require("DappToken");
+const DaiToken = artifacts.require("DaiToken");
 
-module.exports = function(deployer) {
+module.exports = async function(deployer, network, accounts) {
+  // deploy the mock dai token
+  await deployer.deploy(DaiToken)
+  const daiToken = await DaiToken.deployed()
+
   deployer.deploy(Tokenfarm);
 };
--- src/contracts/TokenFarm.sol
+++ src/contracts/TokenFarm.sol
@@ -1,7 +1,17 @@
 pragma solidity ^0.5.0;
 // this contract is responsible for the yielding
+import "./DappToken";
+import "./DaiToken";
 
 contract TokenFarm {
   string public name = "Dapp Token Farm";
+  DappToken public dappToken;
+  DaiToken public daiToken;
   // state varible that gets stored on the block chain
+
+  constructor(DappToken _dappToken, DaiToken _daiToken) {
+    dappToken = _dappToken;
+    daiToken = _daiToken;
+    // grab token addresses and assign to state vars to access them in other funtions
+  }
 }
