fcaf722af4d6a09dc6db760231086ef2658776c9
Sahil Athia <sahilathia918@gmail.com>
2021-02-23 16:34:42 -0500

    add deployment scripts and transfer tokens

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,4 +1,4 @@
-const Tokenfarm = artifacts.require("Tokenfarm");
+const TokenFarm = artifacts.require("TokenFarm");
 const DappToken = artifacts.require("DappToken");
 const DaiToken = artifacts.require("DaiToken");
 
@@ -7,5 +7,19 @@ module.exports = async function(deployer, network, accounts) {
   await deployer.deploy(DaiToken)
   const daiToken = await DaiToken.deployed()
 
-  deployer.deploy(Tokenfarm);
+  // deploy the dapp token
+  await deployer.deploy(DappToken)
+  const dappToken = await DappToken.deployer()
+
+  // deploy the token farm
+  await deployer.deploy(Tokenfarm, dappToken.address, daiToken.address)
+  const tokenFarm = await TokenFarm.deployed()
+
+  // we are going to add all of th dapp tokens to the token farm
+  // to give them as intrest on DAI investment
+
+  // trander all 1 million dapp coins
+  // token has 18 decimal places, thats why it looks like theres more than 1 million
+  // solidity does not handle decimals so it has to be stored as an integer
+  await dappToken.transfer(tokenFarm.address, '1000000000000000000000000')
 };
