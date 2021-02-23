4415436aa7b251df00af4a6c9539ec81a7ddcd13
Sahil Athia <sahilathia918@gmail.com>
2021-02-23 14:11:08 -0500

    initialize smart contract for yielding

---
+++ src/contracts/TokenFarm.sol
@@ -0,0 +1,7 @@
+pragma solidity ^0.5.0;
+// this contract is responsible for the yielding
+
+contract TokenFarm {
+  string public name = "Dapp Token Farm";
+  // state varible that gets stored on the block chain
+}
--- truffle-config.js
+++ truffle-config.js
@@ -2,9 +2,10 @@ require('babel-register');
 require('babel-polyfill');
 
 module.exports = {
+  // this connects the trubble project to the blockchain (ganache 7545)
   networks: {
     development: {
-      host: "127.0.0.1",
+      host: "127.0.0.1", // same as localhost
       port: 7545,
       network_id: "*" // Match any network id
     },
