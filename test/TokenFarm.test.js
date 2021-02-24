dd5d5254ad31e36015dee6a94d1c37f28e59b736
Sahil Athia <sahilathia918@gmail.com>
2021-02-24 15:31:37 -0500

    test for correct balance before stake

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -56,4 +56,14 @@ contract("TokenFarm", ([owner, investor]) => {
       assert.equal(balance.toString(), tokens("1000000"))
     })
   })
+
+  describe("Farming Tokens", async () => {
+    it("rewards investors for staking in DAI tokens", async () => {
+      let result;
+
+      // check investor balance before staking
+      result = await daiToken.balanceOf(investor)
+      assert.equal(result.toString(), tokens("100"), "investor mock DAI token balance should be correct before deposit")
+    })
+  })
 })
