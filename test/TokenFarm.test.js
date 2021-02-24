0ad2632dd42a186b4ac3c349c5000b0951618076
Sahil Athia <sahilathia918@gmail.com>
2021-02-23 23:18:01 -0500

    remove test error

--- test/TokenFarm.test.js
+++ test/TokenFarm.test.js
@@ -1,5 +1,3 @@
-const { Item } = require("react-bootstrap/lib/Breadcrumb");
-
 const DaiToken = artifacts.require("DaiToken");
 const DappToken = artifacts.require("DappToken");
 const TokenFarm = artifacts.require("TokenFarm");
@@ -14,7 +12,7 @@ contract("TokenFarm", (accounts) => {
     it("has a name", async () => {
       let daiToken = await DaiToken.new()
       const name = await daiToken.name()
-      assert.equal(name, "Mock Dai Token")
+      assert.equal(name, "Mock DAI Token")
     })
   })
 })
