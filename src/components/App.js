6adec5ff3676e0e3461815f3ef588ae44da152af
Sahil Athia <sahilathia918@gmail.com>
2021-02-28 14:07:06 -0500

    load web3 function

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
 import './App.css'
+import Web3 from 'web3'
 
 class App extends Component {
+
+  async load_web3() {
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum)
+      await window.ethereum.enable()
+    } else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider)
+    } else {
+      window.alert("Non-ethereum browser detected. Try using MetaMask")
+    }
+  }
 
   constructor(props) {
     super(props)
@@ -19,14 +31,8 @@ class App extends Component {
           <div className="row">
             <main role="main" className="col-lg-12 ml-auto mr-auto" style={{ maxWidth: '600px' }}>
               <div className="content mr-auto ml-auto">
-                <a
-                  href="http://www.dappuniversity.com/bootcamp"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                </a>
 
-                <h1>Hello, World!</h1>
+                <h1>Hello, World</h1>
 
               </div>
             </main>
