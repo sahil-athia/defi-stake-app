fb6faf4e17cfe641e010266db9f6f9065f851023
Sahil Athia <sahilathia918@gmail.com>
2021-03-01 21:26:35 -0500

    show balancees

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,7 +4,21 @@ class Main extends Component {
   render() {
     return (
       <div id="content" className="mt-3">
-        <h1>this is the main compoenet</h1>
+        <table className="table table-borderless text-muted text-center">
+          <thead>
+            <tr>
+              <th scope="col">Staking Balance</th>
+              <th scope="col">Reward Balance</th>
+            </tr>
+          </thead>
+
+          <tbody>
+            <tr>
+              <td>{window.web3.utils.fromWei(this.props.stakingBalance)} mDAI</td>
+              <td>{window.web3.utils.fromWei(this.props.dappTokenBalance)} DApp</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
     )
   }
