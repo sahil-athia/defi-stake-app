8cf3ac2731838bef3d9e5fa8aeaa3beb47d6bb66
Sahil Athia <sahilathia918@gmail.com>
2021-03-01 22:00:25 -0500

    add staking function in app

--- src/components/App.js
+++ src/components/App.js
@@ -92,6 +92,15 @@ class App extends Component {
     } else {
       window.alert("Non-ethereum browser detected. Try using MetaMask")
     }
+
+    stakeTokens = (amount) => {
+      this.setState({ loading: true })
+      this.state.daiToken.methods.approve(this.state.tokenFarm._address, amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+        this.state.tokenFarm.methods.stakeTokens(amount).send({ from: this.state.account }).on('transactionHash', (hash) => {
+          this.setState({ loading: false })
+        })
+      })
+    }
   }
 
 
--- src/components/Main.js
+++ src/components/Main.js
@@ -39,6 +39,7 @@ class Main extends Component {
               <div className="input-group mb-4">
                 <input
                   type="text"
+                  ref={(input) => { this.input = input }}
                   className="form-control form-control-lg"
                   placeholder="0"
                   required
